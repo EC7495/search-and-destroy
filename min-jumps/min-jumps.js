05491b0c13b08cfdc6c6ca7e6fff64cb8e79c801
Stanislav Levitt <stanislavlevitt@yahoo.com>
2020-02-19 17:52:02 -0500

    jump

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,18 @@
 
 // Complete this algo
 const minJumps = arr => {
+  let start = arr[0]
+  let startIndx = 0
+  let minJump =0
 
+  while(startIndx < arr.length-1){
+     start = Math.max(...arr.slice(startIndx +1, start +1))
+     startIndx = arr.indexOf(start,startIndx)
+     minJump++
+  }
+
+
+  return minJump
 };
 
 module.exports = minJumps
