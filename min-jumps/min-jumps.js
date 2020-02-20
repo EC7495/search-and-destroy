a175a56195284f6d58503fadbf6e10420bf9ce94
Erick <ecanals07@gmail.com>
2020-02-19 20:38:28 -0500

    checking infinite loop bug

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,18 +2,21 @@
 
 // Complete this algo
 const minJumps = arr => {
-  let start = arr[0]
-  let startIndx = 0
-  let minJump =0
+  let start = arr[0];
+  let startIndx = 0;
+  let minJump = 0;
 
-  while(startIndx < arr.length-1){
-     start = Math.max(...arr.slice(startIndx +1, start +1))
-     startIndx = arr.indexOf(start,startIndx)
-     minJump++
+  while (startIndx < arr.length - 1) {
+    debugger;
+    if (start > arr.length) return 1;
+    start = Math.max(...arr.slice(startIndx + 1, start + 1));
+    startIndx = arr.indexOf(start, startIndx);
+    minJump++;
   }
 
-
-  return minJump
+  return minJump;
 };
 
-module.exports = minJumps
+minJumps([2, 4, 1, 1, 2, 3, 7, 1, 1, 3]);
+
+module.exports = minJumps;
